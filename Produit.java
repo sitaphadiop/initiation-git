53db4220f01078187a3bb1cc228927c7f91b57a5
Sitapha <sitaphadiop30@gmail.com>
2024-11-11 08:23:00 +0100

    test

--- Produit.java
+++ Produit.java
@@ -3,5 +3,7 @@ public class Produit {
         int A = 3, B = 5, P;
         P = (A * B); // erreur ; à la place de :
         System.out.println("le produit de " + A + " et de " + B + " = " + P);
+        // test
+
     }
 }
