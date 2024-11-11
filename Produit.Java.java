892c060808a891c55813c2b70dd32f8d3cc44062
Sitapha <sitaphadiop30@gmail.com>
2024-11-11 08:16:21 +0100

    first commit

---
+++ Produit.Java.java
@@ -0,0 +1,7 @@
+public class Produit {
+    public static void main(String[] args) {
+        int A = 3, B = 5, P;
+        P = (A * B); // erreur ; à la place de :
+        System.out.println("le produit de " + A + " et de " + B + " = " + P);
+    }
+}
